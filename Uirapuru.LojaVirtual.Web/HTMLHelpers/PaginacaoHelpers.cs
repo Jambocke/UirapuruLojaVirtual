30c32eb7facbfdefcc815eb157f011f8e3235663
Jambocke <davidprll@gmail.com>
2016-07-18 00:58:11 -0300

    Classe de Paginacao

---
+++ Uirapuru.LojaVirtual.Web/HTMLHelpers/PaginacaoHelpers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Uirapuru.LojaVirtual.Web.Models;
+
+namespace Uirapuru.LojaVirtual.Web.HTMLHelpers
+{
+    public static class PaginacaoHelpers
+    {
+        public static  MvcHtmlString PageLinks(this HtmlHelper html, Paginacao paginacao, Func<int, string> paginarUrl)
+        {
+            StringBuilder resultado = new StringBuilder();
+
+            for (int i = 0; i < paginacao.totalPagina; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.MergeAttribute("href", paginarUrl(i));
+                tag.InnerHtml = i.ToString();
+
+                if (i.Equals(paginacao.pagAtual))
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+                }
+                tag.AddCssClass("btn btn-default");
+                resultado.Append(tag);
+            }
+            return MvcHtmlString.Create(resultado.ToString());
+        }
+    }
+}
---
+++ Uirapuru.LojaVirtual.Web/Models/Paginacao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Uirapuru.LojaVirtual.Web.Models
+{
+    public class Paginacao
+    {
+        public int itensTotal{ get; set; }
+        public int itensPorPagina { get; set; }
+        public int pagAtual { get; set; }
+
+        public int totalPagina
+        {
+            get
+            {
+                return (int)Math.Ceiling((decimal)itensTotal / itensPorPagina);
+            }
+        }
+    }
+}
